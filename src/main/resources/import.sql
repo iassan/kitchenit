89394dd6e932c751038c26dc71cb3eea77f11ae3
Jacek Bilski <jacek.bilski@gmail.com>
2016-08-21 14:03:05 +0200

    Dropped concept of user, also from DB.

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,3 @@
-INSERT INTO user (USERNAME) VALUES ('thor');
-INSERT INTO user (USERNAME) VALUES ('loki');
-INSERT INTO shopping_list (list_name, owner_username) VALUES ('thor1', 'thor');
-INSERT INTO shopping_list (list_name, owner_username) VALUES ('thor2', 'thor');
-INSERT INTO shopping_list (list_name, owner_username) VALUES ('loki1', 'loki');
+INSERT INTO shopping_list (list_name) VALUES ('thor1');
+INSERT INTO shopping_list (list_name) VALUES ('thor2');
+INSERT INTO shopping_list (list_name) VALUES ('loki1');
